696020a20a1a8b882aa8780b98d98a56cf5afc1b
rh2740 <rh2740@nyu.edu>
2018-08-24 10:12:04 -0400

    08.24.1

--- mainm.py
+++ mainm.py
@@ -220,7 +220,6 @@ def CAM(net, w, pred, x, y, path, name):
             ascending_order = np.argsort(scoresMean)
             IDX_category = ascending_order[::-1]  # [::-1] to sort in descending order
             print(weights_LR.shape)
-            print(weights_LR[0, :])
             print(weights_LR[[0], :])
             print(weights_LR[[1], :])
             curCAMmapAll = py_returnCAMmap(activation_lastconv, weights_LR[[1], :])
@@ -230,26 +229,26 @@ def CAM(net, w, pred, x, y, path, name):
                 curHeatMap = cv2.resize(im2double(curCAMmapLarge_crops), (299, 299))  # this line is not doing much
                 curHeatMap = im2double(curHeatMap)
                 curHeatMap = py_map2jpg(curHeatMap, None, 'jet')
-                print(x[ij].shape)
                 xim = x[ij].reshape(-1, 3)
-                print(xim.shape)
                 xim1 = xim[:, 0].reshape(-1, 299)
                 xim2 = xim[:, 1].reshape(-1, 299)
                 xim3 = xim[:, 2].reshape(-1, 299)
-                print(xim1.shape)
                 image = np.empty([299,299,3])
                 image[:, :, 0] = xim1
                 image[:, :, 1] = xim2
                 image[:, :, 2] = xim3
-                print(image.shape)
                 # image = cv2.resize(x[ij], (299, 299, 3))
-                a = im2double(image) * 0.2
-                b = im2double(curHeatMap) * 0.7
+                a = im2double(image)
+                b = im2double(curHeatMap)
                 print(a.shape)
                 print(b.shape)
                 curHeatMap = im2double(image) * 0.2 + im2double(curHeatMap) * 0.7
                 imname = DIR + '/' + str(ij) + '.png'
+                imname1 = DIR + '/' + str(ij) + 'img.png'
+                imname2 = DIR + '/' + str(ij) + 'hm.png'
                 cv2.imwrite(imname, curHeatMap)
+                cv2.imwrite(imname1, a)
+                cv2.imwrite(imname2, b)
 
 
 def main(to_reload=None, test=None, log_dir=None):
