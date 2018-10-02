0b5b67fda428c5f3153f1de4503f7acbbcf05e01
rh2740 <rh2740@nyu.edu>
2018-10-02 17:51:03 -0400

    10.02.01

--- Realtest.py
+++ Realtest.py
@@ -4,3 +4,14 @@ Created on 09/28/2018
 
 @author: RH
 """
+
+import get_tile
+
+# cut tiles with coordinates in the name (exclude white)
+
+get_tile.tile()
+
+# load 5000 each time until it is done
+
+# output heat map of pos and neg; and output CAM and assemble them to a big graph.
+
--- get_tile.py
+++ get_tile.py
@@ -1,79 +1,67 @@
 from openslide import OpenSlide
-from scipy import signal
 import numpy as np
 import pandas as pd
 import os
 
-slide = OpenSlide("../Neutrophil/ImageCollection_0000026280_2016-10-27 14_13_01.scn")
+def tile(path_to_slide = "../Neutrophil/", image_file = "ImageCollection_0000026280_2016-10-27 14_13_01.scn"):
+    slide = OpenSlide(path_to_slide+image_file)
 
-assert 'openslide.bounds-height' in slide.properties
-assert 'openslide.bounds-width' in slide.properties
-assert 'openslide.bounds-x' in slide.properties
-assert 'openslide.bounds-y' in slide.properties
+    assert 'openslide.bounds-height' in slide.properties
+    assert 'openslide.bounds-width' in slide.properties
+    assert 'openslide.bounds-x' in slide.properties
+    assert 'openslide.bounds-y' in slide.properties
 
-x = int(slide.properties['openslide.bounds-x'])
-y = int(slide.properties['openslide.bounds-y'])
-bounds_height = int(slide.properties['openslide.bounds-height'])
-bounds_width = int(slide.properties['openslide.bounds-width'])
+    x = int(slide.properties['openslide.bounds-x'])
+    y = int(slide.properties['openslide.bounds-y'])
+    bounds_height = int(slide.properties['openslide.bounds-height'])
+    bounds_width = int(slide.properties['openslide.bounds-width'])
 
-# bounds_x = x + bounds_width
-# bounds_y = y + bounds_height
+    half_width_region = 49
+    full_width_region = 299
+    stepsize = full_width_region - half_width_region
 
-half_width_region = 149
-full_width_region = 2 * half_width_region + 1
+    n_x = int((bounds_width - 1) / stepsize)
+    n_y = int((bounds_height - 1) / stepsize)
 
-n_x = int((bounds_width - 1) / half_width_region)
-n_y = int((bounds_height - 1) / half_width_region)
+    dat = np.empty((0, int(299 ** 2 * 3)), dtype='uint8')
 
-coords = pd.read_table('../Neutrophil/all_users_reduced_curation_compressed.txt', delimiter='\t',
-                       header=0)
-pos = coords.loc[(coords['State'] == '+') & (coords['Slide'] == 'Slide80.scn')]
+    imloc = []
+    counter = 0
+    svcounter = 0
 
-x_edge = np.arange(n_x + 1) * 149
-y_edge = np.arange(n_y + 1) * 149
+    if not os.path.exists(path_to_slide + 'Tiles'):
+            os.makedirs(path_to_slide + 'Tiles')
 
-lab, _, _ = np.histogram2d(x=np.asarray(pos['X']),
-                           y=np.asarray(pos['Y']),
-                           bins=[x_edge, y_edge])
+    for i in range(n_x - 1):
+        for j in range(n_y - 1):
+            target_x = stepsize * i
+            target_y = stepsize * j
 
-lab_res = signal.convolve2d(lab, np.ones((2, 2)), mode='valid').astype(int)
+            image_x = target_x + x
+            image_y = target_y + y
 
-dat = np.empty((0, int(299 ** 2 * 3)), dtype='uint8')
-tile_lab = []
+            the_image = slide.read_region((image_x, image_y), 0, (full_width_region, full_width_region))
+            the_imagea = np.array(the_image)[:,:,:3]
 
-if not os.path.exists('../Neutrophil/Tiles'):
-        os.makedirs('../Neutrophil/Tiles')
+            mask = (the_imagea[:,:,:3] > 200).astype(np.uint8)
+            mask = mask[:,:,0]*mask[:,:,1]*mask[:,:,2]
+            white = np.sum(mask)/(299*299)
 
-for i in range(n_x - 1):
-    for j in range(n_y - 1):
-        target_x = half_width_region * i
-        target_y = half_width_region * j
+            if white < 0.5:
+                the_image.save(path_to_slide+"Tiles/region_x-{}-y-{}.png".format(target_x, target_y))
+                imloc.append([svcounter, counter, target_x, target_y])
+                if svcounter % 5000 == 0:
+                    np.savetxt(path_to_slide+'data-{}.txt'.format(svcounter), dat, fmt='%i', delimiter='\t')
+                    dat = np.empty((0, int(299 ** 2 * 3)), dtype='uint8')
+                pix = np.array(the_image)[:, :, 0:3]
+                dat = np.vstack([dat, pix.flatten()])
+                svcounter += 1
+            else:
+                print('Ignore white!')
 
-        image_x = target_x + x
-        image_y = target_y + y
+            counter += 1
 
-        the_image = slide.read_region((image_x, image_y), 0, (full_width_region, full_width_region))
-        the_imagea = np.array(the_image)[:,:,:3]
-
-        mask = (the_imagea[:,:,:3] > 200).astype(np.uint8)
-        mask = mask[:,:,0]*mask[:,:,1]*mask[:,:,2]
-        white = np.sum(mask)/(299*299)
-        # print(white)
-
-        if white < 0.5:
-            the_image.save("../Neutrophil/Tiles/region_x{}_y{}_{}.png".format(format(i, '02d'),
-                                                                                                    format(j, '02d'),
-                                                                                                    format(
-                                                                                                        lab_res[i, j],
-                                                                                                        '02d')))
-            pix = np.array(the_image)[:, :, 0:3]
-            dat = np.vstack([dat, pix.flatten()])
-            tile_lab.append((lab_res[i, j] > 0))
-        else:
-            print('Ignore white!')
-
-tile_lab = np.asarray(tile_lab).astype(int)
-np.savetxt('../Neutrophil/slide80_mini_data.txt', dat, fmt='%i', delimiter='\t')
-np.savetxt('../Neutrophil/slide80_mini_lab.txt', tile_lab, fmt='%i', delimiter='\t')
-
-# the_image.save("region.png")
+    np.savetxt(path_to_slide+'slide80_mini_data-{}.txt'.format(svcounter), dat, fmt='%i', delimiter='\t')
+    dat = np.empty((0, int(299 ** 2 * 3)), dtype='uint8')
+    imlocpd = pd.DataFrame(imloc, columns = ["Num", "Count", "X", "Y"])
+    imlocpd.to_csv(path_to_slide+"dict.csv", index = False)
