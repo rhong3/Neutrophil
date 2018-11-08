21981880f0316a092fd84cbf959a69c0370363d0
rh2740 <rh2740@nyu.edu>
2018-11-08 16:41:31 -0500

    11.08.1

--- get_tilev3.py
+++ get_tilev3.py
@@ -16,11 +16,12 @@ def bgcheck(img):
     return white
 
 
-def v_slide(slp, outdir, n_y, x, y, tile_size, stepsize, x0):
+def v_slide(slp, n_y, x, y, tile_size, stepsize, x0):
     # pid = os.getpid()
     # print('{}: start working'.format(pid))
     slide = OpenSlide(slp)
     imloc = []
+    imlist = []
     y0 = 0
     target_x = x0 * stepsize
     image_x = target_x + x
@@ -30,12 +31,11 @@ def v_slide(slp, outdir, n_y, x, y, tile_size, stepsize, x0):
         img = slide.read_region((image_x, image_y), 0, (tile_size, tile_size))
         wscore = bgcheck(img)
         if wscore < 0.5:
-            img.save(outdir + "/region_x-{}-y-{}.png".format(target_x, target_y))
-            strr = outdir + "/region_x-{}-y-{}.png".format(target_x, target_y)
-            imloc.append([x0, y0, target_x, target_y, strr])
+            imloc.append([x0, y0, target_x, target_y])
+            imlist.append(np.array(img))
         y0 += 1
     slide.close()
-    return imloc
+    return imloc, imlist
 
 
 def tile(image_file, outdir, path_to_slide = "../Neutrophil/"):
@@ -64,61 +64,42 @@ def tile(image_file, outdir, path_to_slide = "../Neutrophil/"):
     lowres = slide.read_region((x, y), 2, (int(n_x*stepsize/16), int(n_y*stepsize/16)))
     lowres = np.array(lowres)[:,:,:3]
 
-    if not os.path.exists(outdir):
-            os.makedirs(outdir)
-    slide.close()
-
     x0 = 0
     # create multiporcessing pool
     print(mp.cpu_count())
     pool = mp.Pool(processes=mp.cpu_count())
     tasks = []
     while x0 < n_x:
-        task = tuple((slp, outdir, n_y, x, y, full_width_region, stepsize, x0))
+        task = tuple((slp, n_y, x, y, full_width_region, stepsize, x0))
         tasks.append(task)
         x0 += 1
     # slice images with multiprocessing
     temp = pool.starmap(v_slide, tasks)
+    tempdict = list(zip(*temp))[0]
+    tempimglist = list(zip(*temp))[1]
+    temp = None
     pool.close()
     pool.join()
 
-    temp = list(filter(None, temp))
+    tempdict = list(filter(None, tempdict))
     imloc = []
-    list(map(imloc.extend, temp))
-    imlocpd = pd.DataFrame(imloc, columns = ["X_pos", "Y_pos", "X", "Y", "Loc"])
+    list(map(imloc.extend, tempdict))
+    imlocpd = pd.DataFrame(imloc, columns = ["X_pos", "Y_pos", "X", "Y"])
     imlocpd = imlocpd.sort_values(["X_pos", "Y_pos"], ascending=[True, True])
     imlocpd = imlocpd.reset_index(drop=True)
     imlocpd = imlocpd.reset_index(drop=False)
-    imlocpd.columns = ["Num", "X_pos", "Y_pos", "X", "Y", "Loc"]
+    imlocpd.columns = ["Num", "X_pos", "Y_pos", "X", "Y"]
     imlocpd.to_csv(outdir + "/dict.csv", index = False)
+    tempdict = None
 
-    return n_x, n_y, lowres, residue_x, residue_y
+    tempimglist = list(filter(None, tempimglist))
+    imglist = []
+    list(map(imglist.extend, tempimglist))
+    print(len(imglist))
+    tempimglist = None
+    imglist = np.asarray(imglist)
 
 
-def sz(image_file, path_to_slide = "../Neutrophil/"):
-    slide = OpenSlide(path_to_slide+image_file)
+    return n_x, n_y, lowres, residue_x, residue_y, imglist
 
-    assert 'openslide.bounds-height' in slide.properties
-    assert 'openslide.bounds-width' in slide.properties
-    assert 'openslide.bounds-x' in slide.properties
-    assert 'openslide.bounds-y' in slide.properties
-
-    x = int(slide.properties['openslide.bounds-x'])
-    y = int(slide.properties['openslide.bounds-y'])
-    bounds_height = int(slide.properties['openslide.bounds-height'])
-    bounds_width = int(slide.properties['openslide.bounds-width'])
-
-    half_width_region = 49
-    full_width_region = 299
-    stepsize = full_width_region - half_width_region
-
-    n_x = int((bounds_width - 1) / stepsize)
-    n_y = int((bounds_height - 1) / stepsize)
-
-    residue_x = int((bounds_width - n_x * stepsize)/50)
-    residue_y = int((bounds_height - n_y * stepsize)/50)
-    lowres = slide.read_region((x, y), 2, (int(n_x * stepsize/16), int(n_y * stepsize/16)))
-    lowres = np.array(lowres)[:, :, :3]
-
-    return n_x, n_y, lowres, residue_x, residue_y
 
